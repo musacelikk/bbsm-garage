435c601c221c889f868cf2b9d262f9b768357723
Musa Çelik <musacelik@192.168.1.75>
2025-11-30 20:03:27 +0300

    ffinshcommit

--- bbsm-garage-back/src/main.ts
+++ bbsm-garage-back/src/main.ts
@@ -9,13 +9,14 @@ async function bootstrap() {
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.enableCors({
-    origin: '*',
+    origin: process.env.FRONTEND_URL || '*',
     allowedHeaders: '*',
     methods: '*',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
 
-  await app.listen(4000);
+  const port = process.env.PORT || 4000;
+  await app.listen(port);
 }
 bootstrap();
